2ef1e4fbb3526a36ddb167882db4472c0c7e86c1
jeeva2812 <jeevaforchess@gmail.com>
2020-05-12 16:18:36 +0530

    Improved the slider

--- src/App.js
+++ src/App.js
@@ -1,50 +1,77 @@
 import React, { useState } from 'react';
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player';
+
+import Slider from '@material-ui/core/Slider';
+import IconButton from '@material-ui/core/IconButton';
+
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 
 function App() {
 
-  const [playing, setPlaying] = useState(false)
-  const [played, setPlayed] = useState(0)
-  const [player, setPlayer] = useState()
+  const [playing, setPlaying] = useState(false);
+  const [played, setPlayed] = useState(0);
+  const [player, setPlayer] = useState();
 
+  const marks = [];
 
-  const handleSeekMouseDown = e => {
-    //this.setState({ seeking: true })
+  for(let i=1;i<=12;i++){
+    let x = {
+      value: i*60*5,
+      label: `${i*5} minutes` 
+    };
+    marks.push(x);
   }
 
-  const handleSeekChange = e => {
-    setPlayed(parseFloat(e.target.value))
+  const handleSeekChange = (event, newValue) => {
+    setPlayed(newValue)
+    player.seekTo(newValue,'seconds')
   }
 
-  const handleSeekMouseUp = e => {
-    //this.setState({ seeking: false })
-    player.seekTo(parseFloat(e.target.value))
+  const handlePlay = () => {
+    setPlaying(true)
+  }
+
+  const handlePause = () => {
+    setPlaying(false)
   }
 
   const handlePlayPause = () => {
     setPlaying(!playing)
   }
 
-  const ref = x => {
-    setPlayer(x)
+  const handleProgress = state => {
+    setPlayed(state.playedSeconds)
+  }
+
+  const getTime = x => {
+    return `${Math.round(x/60)}`
+  }
+
+  const ref = player => {
+    setPlayer(player)
   }
 
   return (
     <div className="App">
       <ReactPlayer 
         ref = {ref}
-        url='./assets/test.mp4' 
+        url= './assets/test.mp4'
         playing = {playing}
+        onPlay={handlePlay}
+        onPause={handlePause}
+        onProgress= {handleProgress}
          />
 
-      <button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
+      <IconButton onClick={handlePlayPause}>{playing ? <PauseIcon />  : <PlayArrowIcon />}</IconButton>
 
-      <input
-        type='range' min={0} max={1} step={0.083333}
+      <Slider
         value={played}
-        onMouseDown={handleSeekMouseDown}
-        onChange={handleSeekChange}
-        onMouseUp={handleSeekMouseUp}
+        max = {3600}
+        onChange = {handleSeekChange}
+        valueLabelDisplay = "auto"
+        valueLabelFormat = {getTime}
+        marks = {marks}
       />
 
     </div>
